d6eddbe834a77146addf74980eb005d52198f0e1
otoukebri <oussema.toukebri@gmail.com>
2019-12-02 21:37:51 +0100

    init project skeleton

---
+++ payment-service/src/main/java/tn/zelda/App.java
@@ -0,0 +1,13 @@
+package tn.zelda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
